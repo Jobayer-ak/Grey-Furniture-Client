b1f46ca50141bc29e3150a9bffa4a0c6aab795c9
Jobayer_ak <jobayer.ak@gmail.com>
2023-09-03 00:39:12 +0600

    working on hero section

--- src/layout/Navbar.tsx
+++ src/layout/Navbar.tsx
@@ -1,90 +1,79 @@
 import { Link } from 'react-router-dom';
-import { MdOutlineKeyboardArrowDown, MdOutlineKeyboardArrowRight } from 'react-icons/md';
+import { MdOutlineKeyboardArrowDown } from 'react-icons/md';
 import { BsSearch } from 'react-icons/bs';
 import { AiOutlineShoppingCart } from 'react-icons/ai';
 import { CgProfile } from 'react-icons/cg';
-import { useState } from 'react';
 
 function Navbar() {
-  const [open, setOpen] = useState(false);
-
-  const handleMouseEnter = () => {
-    setOpen(true);
-  };
-
-  const handleMouseLeave = () => {
-    setOpen(false);
-  };
-
-  const handleMouseOver = () => {
-    setOpen(true);
-  };
-
-  console.log('Open:', open);
-
   return (
-    <div className="mx-[65px] sticky z-[50]">
-      <div className=" flex gap-8  pt-6">
-        <div className="mt-0 ">
-          <h3 className="text-2xl text-white font-bold italic bg-[#de885f] rounded-md">
-            GREY
-          </h3>
-        </div>
-        <ul className="flex gap-8 text-xl p-2">
-          <li className="flex items-center gap-1 p-4 pt-2 text-white hover:text-black hover:bg-white relative group">
-            <Link to="/all-collections">All Collection</Link>
-            <div>
-              <MdOutlineKeyboardArrowDown size={25} />
-            </div>
+    <div className="w-full sticky z-[50]">
+      <div className="h-full w-full flex justify-end gap-8  pt-12">
+        <div className="flex items-center justify-start gap-8 w-full h-full mx-[60px]">
+          <div>
+            <h3 className="text-2xl text-white font-bold italic bg-[#de885f] rounded-md">
+              GREY
+            </h3>
+          </div>
 
-            <ul className="absolute mt-[-99999px] left-0 pl-3 pb-2 text-black group-hover:mt-[178px] w-full bg-white group-hover:translate-y-10 transform transition duration-300 ease-in-out">
-              <li className="p-1 pt-3 hover:translate-x-3 transform transition duration-300 ease-in-out">
-                <h5 className="">Chair</h5>
+          <div>
+            <ul className="flex gap-5 text-xl">
+              <li className="py-2 px-4 text-white hover:text-black hover:bg-white relative group">
+                <div className="flex items-center">
+                  <Link to="/all-collections">All Collection</Link>
+
+                  <MdOutlineKeyboardArrowDown size={25} className="ml-1" />
+                </div>
+
+                <ul className="absolute mt-[-99999px] left-0 pl-3 pb-2 text-black group-hover:mt-[0px] w-full bg-white group-hover:translate-y-2 border-t-[1px]  transform transition duration-300 ease-in-out">
+                  <li className="p-2 hover:translate-x-3 transform transition duration-300 ease-in-out">
+                    <h5 className="">Chair</h5>
+                  </li>
+                  <li className="p-2 hover:translate-x-3 transform transition duration-300 ease-in-out group relative">
+                    <h5 className="flex items-center ">Table</h5>
+                  </li>
+                  <li className="p-2 hover:translate-x-3 transform transition duration-300 ease-in-out">
+                    <h5 className="">Sofa</h5>
+                  </li>
+                  <li className="p-2 hover:translate-x-3 transform transition duration-300 ease-in-out">
+                    <h5 className="">Storage</h5>
+                  </li>
+                  <li className="p-2 hover:translate-x-3 transform transition duration-300 ease-in-out">
+                    <h5 className="">All Collection</h5>
+                  </li>
+                </ul>
               </li>
-              <li className="p-1 hover:translate-x-3 transform transition duration-300 ease-in-out group relative">
-                <h5 className="flex items-center ">Table</h5>
+              <li className="py-2 px-4 text-white">
+                <Link to="/elevating-desk">Elevating Desk Series</Link>
               </li>
-              <li className="p-1 hover:translate-x-3 transform transition duration-300 ease-in-out">
-                <h5 className="">Sofa</h5>
+              <li className="py-2 px-4 text-white">
+                <Link to="/line-collection">Line Collection</Link>
               </li>
-              <li className="p-1 hover:translate-x-3 transform transition duration-300 ease-in-out">
-                <h5 className="">Storage</h5>
+              <li className="py-2 px-4 text-white">
+                <Link to="/about">About</Link>
               </li>
-              <li className="p-1 hover:translate-x-3 transform transition duration-300 ease-in-out">
-                <h5 className="">All Collection</h5>
+              <li className="py-2 px-4 text-white">
+                <Link to="/contact">Contact</Link>
               </li>
+              {/* <div className="flex justify-center gap-8 ml-7"> */}
+              <li className="py-2 px-4 ">
+                <Link to="/login" className="">
+                  <CgProfile color="white" size={30} />
+                </Link>
+              </li>
+              <li className="py-2 px-4 ">
+                <Link to="/signup">
+                  <BsSearch color="white" size={26} />
+                </Link>
+              </li>
+              <li className="py-2 px-4">
+                <Link to="/signup">
+                  <AiOutlineShoppingCart color="white" size={30} />
+                </Link>
+              </li>
+              {/* </div> */}
             </ul>
-          </li>
-          <li className="p-1 text-white">
-            <Link to="/elevating-desk">Elevating Desk Series</Link>
-          </li>
-          <li className="p-1 text-white">
-            <Link to="/line-collection">Line Collection</Link>
-          </li>
-          <li className="p-1 text-white">
-            <Link to="/about">About</Link>
-          </li>
-          <li className="p-1 text-white">
-            <Link to="/contact">Contact</Link>
-          </li>
-          {/* <div className="flex justify-center gap-8 ml-7"> */}
-          <li className="p-1 ">
-            <Link to="/login" className="">
-              <CgProfile color="white" size={30} />
-            </Link>
-          </li>
-          <li className="p-1 ">
-            <Link to="/signup">
-              <BsSearch color="white" size={26} />
-            </Link>
-          </li>
-          <li className="p-1">
-            <Link to="/signup">
-              <AiOutlineShoppingCart color="white" size={30} />
-            </Link>
-          </li>
-          {/* </div> */}
-        </ul>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,7 +1,8 @@
 import bgImage from '../assets/grey_hero_image-min_1512x.webp';
-import AOS from "aos";
+import AOS from 'aos';
 import 'aos/dist/aos.css';
 import { useEffect } from 'react';
+import { CgArrowLongRight } from 'react-icons/cg';
 
 function Home() {
   const backgroundImage = `url(${bgImage})`;
@@ -15,14 +16,29 @@ function Home() {
   return (
     <div
       data-aos="zoom-out"
-      className="w-full min-h-screen bg-cover bg-center mt-[-160px] relative"
+      className="w-full min-h-screen bg-cover bg-center mt-[-172px] relative"
       style={{ backgroundImage: backgroundImage }}
     >
-      <div className="absolute inset-0 bg-black opacity-40 z-10"></div>
+      <div className="absolute inset-0 bg-gradient-to-b from-[#121211] opacity-90 z-10"></div>
       <div className="relative z-10 flex justify-center items-center h-screen">
         <div>
-          <h2 className="text-4xl font-bold" data-aos="fade-down">Home Page</h2>
-          <p className="text-xl" data-aos="fade-up">This is some content on top of the image.</p>
+          <h1
+            className="text-7xl font-extrabold text-white"
+            data-aos="fade-down"
+          >
+            Modern Furniture
+          </h1>
+          <div className="flex justify-center items-center mt-3">
+            <h3 className="text-2xl text-white tracking-widest">
+              FOR A BETTER WAY TO WORK
+            </h3>
+          </div>
+          <div className="flex justify-center items-center mt-4 group relative">
+            <button className="flex items-cener justify-center text-2xl text-white border-2 py-2 px-4 group-hover:bg-white hover:text-black">
+              Shop Now
+              < CgArrowLongRight/>
+            </button>
+          </div>
         </div>
       </div>
     </div>
